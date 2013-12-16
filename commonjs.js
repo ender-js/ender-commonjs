feb0b651e8a4636e0c2b5ae21fa3fda024b09447
Andrew McCollum <amccollum@gmail.com>
2013-12-16 08:21:46 -0800

    remove extraneous var

--- commonjs.js
+++ commonjs.js
@@ -102,7 +102,7 @@ Module.prototype._load = function () {
  * @param  {string}                     main      the id of the main module
  */
 Module.createPackage = function (id, modules, main) {
-  var path, task
+  var path
 
   for (path in modules) {
     new Module(id + '/' + path, modules[path])
